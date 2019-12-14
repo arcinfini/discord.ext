6d7aa3419a6059d2718497ed4752e58fdeb9103f
William Thomas <williamthomas1233@gmail.com>
2019-12-13 21:18:55 -0500

    new build

--- build/src/index.d.ts
+++ build/src/index.d.ts
@@ -1,3 +1,5 @@
 export * from "./Ext";
 export { Errors } from "./Errors";
 export * from "./Converters";
+import * as Converters from "./Converters";
+export { Converters };
--- build/src/index.js
+++ build/src/index.js
@@ -2,8 +2,17 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./Ext"));
 var Errors_1 = require("./Errors");
 exports.Errors = Errors_1.Errors;
 __export(require("./Converters"));
+const Converters = __importStar(require("./Converters"));
+exports.Converters = Converters;
